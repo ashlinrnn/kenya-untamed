ceedc41dc84146df1045166e2983c49c1e4a2169
Ashlin Rofina <ashlinrofina@gmail.com>
2026-09-15 12:00:50 +0300

    changes app.jsx code

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() { 
+    return ( 
+        <div> 
+            <h1>Kenya Untamed</h1> 
+            <p>Discover the wild side of Kenya</p>
+        </div>
+    )
+} 
+
+export default App 
